f90e740ef59ab79660a67d0e25a7d4a6fab8886d
alan sebastian <alan@alans-mac-mini.(none)>
2011-12-21 11:55:29 -0800

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politics411::Application.config.secret_token = 'ee4997894f43ad93d00f3fa7db3ed4b68f50f1b7e218f48e66ce83ad2803a2f44796113f7b890f5692beed4117a31de13e100b780280f2009b440aa05a798c1b'
