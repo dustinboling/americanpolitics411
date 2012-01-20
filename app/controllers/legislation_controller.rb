35cdbb8ebdc4266c81a1be961da9bf2f2d2bf4e0
Alan Sebastian <asebastian2@gmail.com>
2012-01-20 15:42:53 -0800

    small fix

--- app/controllers/legislation_controller.rb
+++ app/controllers/legislation_controller.rb
@@ -1,7 +1,9 @@
 class LegislationController < ApplicationController
   def grab_xml
     require 'nokogiri'
-    doc = Nokogiri::XML(open(http://thomas.loc.gov/home/gpoxmlc112/s2002_is.xml))
+    require 'open-uri'
+    doc = Nokogiri::HTML(open("http://thomas.loc.gov/home/gpoxmlc112/s2002_is.xml"))
+    puts doc
   end
   
   def list
