68fff5eab3fb3583fa8ae695d7cc96f83ccf8c4b
Alan Sebastian <asebastian2@gmail.com>
2012-01-23 15:07:46 -0800

    added nodes for legislation->grab_xml

--- app/controllers/legislation_controller.rb
+++ app/controllers/legislation_controller.rb
@@ -2,31 +2,28 @@ class LegislationController < ApplicationController
   def grab_xml
     require 'nokogiri'
     require 'open-uri'
+    
     @doc = Nokogiri::XML(open("http://thomas.loc.gov/home/gpoxmlc112/s2002_is.xml")).remove_namespaces!
     
-    @title = @doc.xpath('//title').map do |t|
-      {'title' => t.xpath('//title').inner_text}
-    end
+    @title = @doc.xpath('//title').inner_text
     
-    @date = @doc.xpath('//action-date').map do |date|
-      {'action-date' => date.xpath('//action-date').inner_text}
-    end
+    @date = @doc.xpath('//action-date').inner_text
     
-    @cosponsors = @doc.xpath('//cosponsor').map do |c|
-      {'cosponsor' => c.xpath('//cosponsor').inner_text}
-    end
+    @congress = @doc.xpath('//congress').inner_text
     
-    @sponsors = @doc.xpath('//sponsor').map do |s|
-      {'sponsor' => s.xpath('//sponsor').inner_text}
-    end
+    @session = @doc.xpath('//session').inner_text
+    
+    @legis_num = @doc.xpath('//legis-num').inner_text
+  
+    @sponsor = @doc.xpath('//sponsor').inner_text
+    
+    @cosponsors = @doc.xpath('//cosponsor').inner_text
     
     @committee_names = @doc.xpath('//committee-name').map do |committee|
       {'committee-name' => committee.xpath('//committee-name').inner_text}
     end
     
-    @body = @doc.xpath('//legis-body').map do |body|
-      {'legis-body' => body.xpath('//legis-body').inner_text}
-    end
+    @body = @doc.xpath('//legis-body').inner_text
     
   end
   
