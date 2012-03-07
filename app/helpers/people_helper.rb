08092554e97ac36ef842244fe87cf0b4b3f5974d
Alan Sebastian <asebastian2@gmail.com>
2012-03-06 16:55:05 -0800

    added federal/state breakdown to contributor#show

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -11,17 +11,33 @@ module PeopleHelper
       @dem_count = @stats.dem_count.to_f
       @rep_count = @stats.rep_count.to_f
       @dem_percent = (@dem_count / (@dem_count + @rep_count)) * 100
-      @rep_percent =( @rep_count / (@dem_count + @rep_count)) * 100
+      @rep_percent = (@rep_count / (@dem_count + @rep_count)) * 100
       if @dem_percent > @rep_percent
-        "<p>#{@dem_percent}% Democratic</p>
-         <p>#{@rep_percent}% Democratic</p>".html_safe
+        "<p>#{@dem_percent}% Democratic (#{@dem_count.to_i})</p>
+         <p>#{@rep_percent}% Republican (#{@rep_count.to_i})</p>".html_safe
       elsif @dem_percent == @rep_percent
-        "<p>#{@dem_percent}% Democratic</p>
-         <p>#{@rep_percent}% Democratic</p>".html_safe
+        "<p>#{@dem_percent}% Democratic (#{@dem_count.to_i})</p>
+         <p>#{@rep_percent}% Republican (#{@rep_count.to_i})</p>".html_safe
       else
-        "<p>#{@rep_percent}% Democratic</p>
-         <p>#{@dem_percent}% Democratic</p>".html_safe
+        "<p>#{@rep_percent}% Republican (#{@rep_count.to_i})</p>
+         <p>#{@dem_percent}% Democratic (#{@dem_count.to_i})</p>".html_safe
       end
+    end
+  end
+  
+  def state_federal_breakdown
+    if @id.nil?
+      "N/A"
+    else
+      @stats = TransparencyData::Client.org_level_breakdown(@id)
+      @fed_amount = @stats.federal_amount
+      @state_amount = @stats.state_amount
+      @fed_count = @stats.federal_count.to_f
+      @state_count = @stats.state_count.to_f
+      @fed_percent = (@fed_count / (@fed_count + @state_count)) * 100
+      @state_percent = (@state_count / (@fed_count + @state_count)) * 100
+      "<p>Federal Contributions: #{sprintf("%0.02f", @fed_percent)}% (#{number_to_currency(@fed_amount)})</p>
+       <p>State Contributions: #{sprintf("%0.02f", @state_percent)}% (#{number_to_currency(@state_amount)})</p>".html_safe
     end
   end
   
