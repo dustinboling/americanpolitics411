21d04f7b836055befd641f1d515787c702cc0a57
alan <asebastian2@gmail.com>
2012-03-27 11:02:47 -0700

    Refactored person controller, contributions now loading for people with numbers in their name

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -52,15 +52,16 @@ class PeopleController < ApplicationController
     
     # Load necessary information from the various APIs
     @pac_contributors = TransparencyData::Client.contributions(
-      :recipient_ft => "#{Person.find_by_id(params[:id]).first_name} #{Person.find_by_id(params[:id]).last_name}", 
-      :cycle => 2011, 
+      :recipient_ft => "#{@person.remove_name_numericality}", 
+      :cycle => 2011,
       :amount => {:gte => 2300},
       :contributor_type => "C")
     @individual_contributors = TransparencyData::Client.contributions(
-        :recipient_ft => "#{Person.find_by_id(params[:id]).first_name} #{Person.find_by_id(params[:id]).last_name}", 
+        :recipient_ft => "#{@person.remove_name_numericality}", 
         :cycle => 2011, 
         :amount => {:gte => 2300},
         :contributor_type => "I")
+    
     # sectors
     @entity = TransparencyData::Client.entities(:search => "#{@person.remove_name_numericality}")
     if @entity.first
