cee3a32a1ad020ccf0acf31b66a819c5622ecd74
alan <asebastian2@gmail.com>
2012-04-19 15:49:52 -0700

    Removed json from religions controller.

--- app/controllers/religions_controller.rb
+++ app/controllers/religions_controller.rb
@@ -1,39 +1,33 @@
 class ReligionsController < ApplicationController
-  
+
   load_and_authorize_resource
   skip_authorize_resource :only => :show
   layout 'admin'
-  
+
   # GET /religions
-  # GET /religions.json
   def index
     @religions = Religion.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @religions }
     end
   end
 
   # GET /religions/1
-  # GET /religions/1.json
   def show
     @religion = Religion.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @religion }
     end
   end
 
   # GET /religions/new
-  # GET /religions/new.json
   def new
     @religion = Religion.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @religion }
     end
   end
 
@@ -43,46 +37,38 @@ class ReligionsController < ApplicationController
   end
 
   # POST /religions
-  # POST /religions.json
   def create
     @religion = Religion.new(params[:religion])
 
     respond_to do |format|
       if @religion.save
         format.html { redirect_to @religion, notice: 'Religion was successfully created.' }
-        format.json { render json: @religion, status: :created, location: @religion }
       else
         format.html { render action: "new" }
-        format.json { render json: @religion.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /religions/1
-  # PUT /religions/1.json
   def update
     @religion = Religion.find(params[:id])
 
     respond_to do |format|
       if @religion.update_attributes(params[:religion])
         format.html { redirect_to @religion, notice: 'Religion was successfully updated.' }
-        format.json { head :ok }
       else
         format.html { render action: "edit" }
-        format.json { render json: @religion.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /religions/1
-  # DELETE /religions/1.json
   def destroy
     @religion = Religion.find(params[:id])
     @religion.destroy
 
     respond_to do |format|
       format.html { redirect_to religions_url }
-      format.json { head :ok }
     end
   end
 end
