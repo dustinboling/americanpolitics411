f3c83c91de6e320883965900183c725edb7ce210
alan <asebastian2@gmail.com>
2012-06-29 09:55:21 -0700

    Touch up styling on legislation page.

--- app/models/legislation.rb
+++ app/models/legislation.rb
@@ -15,4 +15,8 @@ class Legislation < ActiveRecord::Base
     self.introduced_year = self.introduced_date.split('-').shift
   end
 
+  def to_param
+    "#{id} #{bill_number}".parameterize
+  end
+
 end
