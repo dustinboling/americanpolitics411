a4763130074220f596cbaa73d53d129f39666e4b
alan <asebastian2@gmail.com>
2012-07-24 13:15:53 -0700

    Change buttons to skeleton style (from bootstrap)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => 'btn small success')
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => 'button')
   end
   
   def error_messages_for(object)
