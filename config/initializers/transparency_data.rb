8bf0453db31ca5b33b8c21abfbc668eb90da39c5
alan <asebastian2@gmail.com>
2012-08-03 11:14:25 -0700

    Fix dataTable styling

--- config/initializers/transparency_data.rb
+++ config/initializers/transparency_data.rb
@@ -3,5 +3,5 @@ TransparencyData.configure do |config|
  config.api_key = '5cdbb43c42d84ef3b4bde09efa074648'
 end
 
-# Real-time Congress key
+# Real-time Congress key (it's the same)
 Congress.key = '5cdbb43c42d84ef3b4bde09efa074648'
