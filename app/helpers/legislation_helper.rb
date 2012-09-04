c83a99221e3a82669276f197b357a72636c8b5fd
alan <asebastian2@gmail.com>
2012-09-04 13:47:10 -0700

    Add basic summary parser

--- app/helpers/legislation_helper.rb
+++ app/helpers/legislation_helper.rb
@@ -2,5 +2,15 @@ module LegislationHelper
   def remove_hyphens(string)
      string.gsub(/-/, "").gsub(/!/, "").gsub(/'/, "")
   end
+
+  def parse_summary(summary)
+    summary_sp1 = summary.split('. ').join('. <br>') 
+    summary_sp2 = summary_sp1.split('.').join('.<br>')
+    summary_sp3 = summary_sp2.split(': ').join(': <br>')
+    summary_sp4 = summary_sp3.split('(').join('<br>(')
+    summary = summary_sp4.html_safe
+
+    return summary
+  end
 end
 
