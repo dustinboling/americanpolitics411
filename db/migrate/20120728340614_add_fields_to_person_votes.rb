84ffa462c65f3bab2ed946ef154856763f8d9b9b
alan <asebastian2@gmail.com>
2012-09-05 09:42:40 -0700

    Remove conflicting migration (for heroku)

--- db/migrate/20120728340614_add_fields_to_person_votes.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToPersonVotes < ActiveRecord::Migration
-  def change
-    add_column :person_votes, :voted_at, :timestamp
-    add_column :person_votes, :how, :string
-    add_column :person_votes, :result, :string
-  end
-end
