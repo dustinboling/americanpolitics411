120662e800b0a50f014b4037092df10f4d39c878
alan <asebastian2@gmail.com>
2012-09-05 16:15:08 -0700

    Remove contact information from person factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,10 +3,6 @@ FactoryGirl.define do
     first_name Faker::Name.first_name
     middle_name Faker::Name.first_name
     last_name Faker::Name.last_name
-    contact_street_address Faker::Address.street_address(include_secondary = false)
-    contact_city Faker::Address.city
-    contact_zip Faker::Address.zip
-    contact_state Faker::Address.state
     bio "This is a test bio"
     professional_experience "This is some test professional experience"
     literary_work "This is some test literary work"
