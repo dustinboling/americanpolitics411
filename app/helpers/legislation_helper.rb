5e2f3c76727241f96b2ae19f080ec5320eb8ae8c
alan <asebastian2@gmail.com>
2012-09-07 14:27:54 -0700

    Finalize summary parser.

--- app/helpers/legislation_helper.rb
+++ app/helpers/legislation_helper.rb
@@ -4,10 +4,10 @@ module LegislationHelper
   end
 
   def parse_summary(summary)
-    summary_sp1 = summary.split('. ').join('. <br>') 
-    summary_sp2 = summary_sp1.split('.').join('.<br>')
+    summary_sp1 = summary.split('. *?!\)').join('. <br>') 
+    summary_sp2 = summary_sp1.split('.*?!\)').join('.<br>')
     summary_sp3 = summary_sp2.split(': ').join(': <br>')
-    summary_sp4 = summary_sp3.split('(').join('<br>(')
+    summary_sp4 = summary_sp3.split(/\((?=\d)/).join('<br>(')
     summary = summary_sp4.html_safe
 
     return summary
