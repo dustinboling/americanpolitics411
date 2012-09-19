66394a2f74b2bc8639fc9d500a68f5a94f9bcfc7
alan <asebastian2@gmail.com>
2012-09-19 16:11:58 -0700

    Remove amazon gem

--- config/initializers/amazon-ecs.rb
+++ config/initializers/amazon-ecs.rb
@@ -1,5 +1,5 @@
-Amazon::Ecs.configure do |options|
-  options[:associate_tag] = 'ameripolit411-20'
-  options[:AWS_access_key_id] = 'AKIAI3YS3NMLE5EI6HVA'
-  options[:AWS_secret_key] = '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'
-end
+# Amazon::Ecs.configure do |options|
+#   options[:associate_tag] = 'ameripolit411-20'
+#   options[:AWS_access_key_id] = 'AKIAI3YS3NMLE5EI6HVA'
+#   options[:AWS_secret_key] = '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'
+# end
