7e9473d9992d008bf3d112361e6593b907dc6f05
alan <asebastian2@gmail.com>
2012-09-20 13:23:11 -0700

    Add micro thumb size to image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,6 +37,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 244]
   end
 
+  version :micro_thumb do
+    process :resize_to_fill => [50, 61]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
